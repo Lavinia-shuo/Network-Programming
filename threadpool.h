900106118a6696af5fc68288117f3b508a319ab2
Lavinia414 <chenshuo_414@163.com>
2022-02-05 00:02:52 +0800

    1.0

--- threadpool.h
+++ threadpool.h
@@ -7,42 +7,33 @@
 #include <pthread.h>
 #include "locker.h"
 
-// 线程池类，将它定义为模板类是为了代码复用，模板参数T是任务类
-template <typename T>
+template <typename T> //T为任务类
 class threadpool
 {
 public:
-    /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
     threadpool(int thread_number = 8, int max_requests = 10000);
     ~threadpool();
     bool append(T *request);
 
 private:
-    /*工作线程运行的函数，它不断从工作队列中取出任务并执行之*/
     static void *worker(void *arg);
     void run();
 
 private:
-    // 线程的数量
-    int m_thread_number;
+    int m_thread_number; // 线程的数量
 
-    // 描述线程池的数组，大小为m_thread_number
-    pthread_t *m_threads;
+    ，
+        pthread_t *m_threads; //线程池大小
 
-    // 请求队列中最多允许的、等待处理的请求的数量
-    int m_max_requests;
+    int m_max_requests; // 请求队列中最多允许的、等待处理的请求的数量
 
-    // 请求队列
-    std::list<T *> m_workqueue;
+    std::list<T *> m_workqueue; // 请求队列
 
-    // 保护请求队列的互斥锁
-    locker m_queuelocker;
+    locker m_queuelocker; // 请求队列的互斥锁
 
-    // 是否有任务需要处理
-    sem m_queuestat;
+    sem m_queuestat; // 是否有任务需要处理
 
-    // 是否结束线程
-    bool m_stop;
+    bool m_stop; // 是否结束线程
 };
 
 template <typename T>
@@ -61,8 +52,7 @@ threadpool<T>::threadpool(int thread_number, int max_requests) : m_thread_number
         throw std::exception();
     }
 
-    // 创建thread_number 个线程，并将他们设置为脱离线程。
-    for (int i = 0; i < thread_number; ++i)
+    for (int i = 0; i < thread_number; ++i) //设置为脱离线程
     {
         printf("create the %dth thread\n", i);
         if (pthread_create(m_threads + i, NULL, worker, this) != 0)
@@ -89,7 +79,6 @@ threadpool<T>::~threadpool()
 template <typename T>
 bool threadpool<T>::append(T *request)
 {
-    // 操作工作队列时一定要加锁，因为它被所有线程共享。
     m_queuelocker.lock();
     if (m_workqueue.size() > m_max_requests)
     {
